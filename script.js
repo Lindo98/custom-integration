05196c73a47eed9eb221807de5316068c8ae55bb
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-08-07 13:33:20 +0200

    fixed javascript code

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-var _launchButton = document.getElementById('launch-btn'),
-    _loaderDiv = document.getElementById('pp-loader'),
-    _previewDiv = document.getElementById('pp-preview-div');
+(function() {
+     
+    var _launchButton = document.getElementById('launch_btn');
+        _previewDiv = document.getElementById('pp_preview_div');
+        _loaderDiv = document.getElementById('pp_loader_div');
 
-var ppclient = new PitchPrintClient({
-    apiKey: 'key_26edfc2cb57d64ce529a3a9e098c63bd',
-    designId: '3d8f3899904ef2392795c681091600d0',
-    custom: true,
-});
 
-var appValidation = () => {
-    _launchButton.removeAttribute('disabled');
-    _launchButton.onclick =() => ppclient.showApp();
-    _loaderDiv.style.display = 'none';
+
+// this is to disable the launch button until PitchPrint is ready for use
+        _launchButton.setAttribute('disabled', 'disabled');
+
+
+// initializing PitchPrint (use my own API key and design ID)    
+    var ppClient = new PitchPrintClient({
+        apiKey: 'key_26edfc2cb57d64ce529a3a9e098c63bd',
+        designId: 'dcat_2edefcc64e533f8e25c438362e275744',
+        custom: true
+    });
+
+ 
+// This function will run/launch the app once it's ready for use 
+    var appValidated = () => {
+        _launchButton.removeAttribute('disabled');
+        _launchButton.onclick = () => ppClient.showApp();
+        _loaderDiv.style.display = 'none';
+    };
+
+
+    // This function will run when the app has been used and user has saved some projects
+    var projectSaved = (_val) => {
+        let _data = _val.data;
+        if (_data && _data.previews && _data.previews.length) {
+            _previewDiv.innerHTML = _data.previews.reduce((_str, prev) => `${_str}<img src="${_prev}" />`, ''); 
+        }
     }
 
-ppclient.on('app-validation', appValidation);
+    ppClient.on('app-validated', appValidated);
+    ppClient.on('project-saved', projectSaved);
+})();
